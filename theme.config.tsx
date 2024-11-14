84c3aca9f92ead2fc8bac86c6ddee1e0fbd230eb
mairh <ujjwal.mairh@gmail.com>
2024-11-14 18:18:24 +0200

    Initial commit with latest code

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Smart Waste Manegement Docs</span>,
+  project: {
+    link: 'https://github.com/mairh/smart-waste-management-docs',
+  },
+  docsRepositoryBase: 'https://github.com/mairh/smart-waste-management-docs',
+  footer: {
+    text: 'Smart Waste Manegement Docs',
+  },
+}
+
+export default config
